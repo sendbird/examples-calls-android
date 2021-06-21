901ab4663c9dbda938c2fa32224ed0d4043eef5c
Nathan Park <nathan.park@sendbird.com>
2021-06-21 12:51:52 +0900

    Implement Authentication view and logic.

--- DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/AuthenticateActivity.kt
+++ DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/AuthenticateActivity.kt
@@ -1,11 +1,59 @@
 package com.sendbird.calls.examples.directcall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.sendbird.calls.AuthenticateParams
+import com.sendbird.calls.SendBirdCall
+import com.sendbird.calls.SendBirdException
+import com.sendbird.calls.User
+import com.sendbird.calls.handler.AuthenticateHandler
 
 class AuthenticateActivity : AppCompatActivity() {
+    lateinit var applicationIdEt: EditText
+    lateinit var userIdEt: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authenticate)
+        applicationIdEt = findViewById<EditText>(R.id.et_application_id).apply { setText(DEFAULT_APP_ID) }
+        userIdEt = findViewById<EditText>(R.id.et_user_id)
+        findViewById<Button>(R.id.btn_sign_in).setOnClickListener(this::onSignInButtonClicked)
+    }
+
+    private fun onSignInButtonClicked(view: View) {
+        val appId = applicationIdEt.text?.toString()
+        val userId = userIdEt.text?.toString()
+
+        if (appId.isNullOrEmpty()) {
+            showToast("App ID is empty.")
+            return
+        }
+
+        if (userId.isNullOrEmpty()) {
+            showToast("User ID is empty.")
+            return
+        }
+
+        SendBirdCall.init(applicationContext, appId)
+        SendBirdCall.authenticate(AuthenticateParams(userId), object : AuthenticateHandler {
+            override fun onResult(user: User?, e: SendBirdException?) {
+                if (e != null) {
+                    showToast(e.message ?: e.toString())
+                    return
+                }
+
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+    }
+
+    companion object {
+        private const val DEFAULT_APP_ID = "773D12BD-8A51-4DB6-AF33-F59D189F006C"
     }
 }
