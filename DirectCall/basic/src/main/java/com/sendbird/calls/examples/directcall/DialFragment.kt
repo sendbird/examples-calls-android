450c3cd815b39e2616163ebc5956ba7330ff6fe3
Nathan Park <nathan.park@sendbird.com>
2021-06-22 12:32:15 +0900

    Implement calling events and end.

--- DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/DialFragment.kt
+++ DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/DialFragment.kt
@@ -1,15 +1,26 @@
 package com.sendbird.calls.examples.directcall
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.navigation.fragment.findNavController
+import com.sendbird.calls.DialParams
+import com.sendbird.calls.DirectCall
+import com.sendbird.calls.SendBirdCall
+import com.sendbird.calls.SendBirdException
+import com.sendbird.calls.handler.DialHandler
+import com.sendbird.calls.handler.SendBirdCallListener
 
 class DialFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var userIdEditText: EditText
+    lateinit var radioGroup: RadioGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +28,56 @@ class DialFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dial, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.dial_button_dial).setOnClickListener(this::onDialButtonClicked)
+        userIdEditText = view.findViewById(R.id.dial_edit_text_user_id)
+        radioGroup = view.findViewById(R.id.dial_radio_group_call_type)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+        SendBirdCall.addListener(TAG, sendbirdCallListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+        SendBirdCall.removeListener(TAG)
+    }
+
+    private fun onDialButtonClicked(view: View) {
+        val userId = userIdEditText.text?.toString()
+        if (userId.isNullOrEmpty()) {
+            context?.showToast("user ID is empty")
+            return
+        }
+        val isVideoCall = radioGroup.checkedRadioButtonId == R.id.dial_radio_button_video
+        SendBirdCall.dial(DialParams(userId).setVideoCall(isVideoCall), object : DialHandler {
+            override fun onResult(call: DirectCall?, e: SendBirdException?) {
+                call?.callId?.let {
+                    val action = DialFragmentDirections.actionDialFragmentToCallingFragment(it)
+                    findNavController().navigate(action)
+                }
+                e?.let { context?.showToast(it.message ?: it.toString()) }
+            }
+        })
+    }
+
+    private val sendbirdCallListener = object : SendBirdCallListener() {
+        override fun onRinging(call: DirectCall) {
+            Log.d(TAG, "onRinging() called with: call = $call")
+            call.callId?.let {
+                val action = DialFragmentDirections.actionDialFragmentToCallingFragment(it)
+                findNavController().navigate(action)
+            }
+        }
+    }
+
+    companion object {
+        val TAG = DialFragment::class.java.simpleName
     }
 }
--- DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/MainActivity.kt
+++ DirectCall/basic/src/main/java/com/sendbird/calls/examples/directcall/MainActivity.kt
@@ -2,29 +2,15 @@ package com.sendbird.calls.examples.directcall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var entryFragmentType: FragmentType
-    private val dialFragment = DialFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         entryFragmentType = intent.extras?.getSerializable(INTENT_EXTRA_ENTRY_FRAGMENT_TYPE) as? FragmentType ?: FragmentType.DIAL
-        transactFragment(entryFragmentType)
     }
-
-    private fun transactFragment(type: FragmentType) {
-        val fragment = when (type) {
-            FragmentType.DIAL -> dialFragment
-            FragmentType.CALLING -> CallingFragment()
-        }
-
-        supportFragmentManager.beginTransaction().add(R.id.main_frame_layout, fragment).commit()
-    }
-
     enum class FragmentType {
         DIAL, CALLING
     }
